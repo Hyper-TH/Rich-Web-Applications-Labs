d09b3a21d5325bd39bc8e2dc7b33804a54f833a7
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-12 19:31:37 +0100

    Improved getResponse() and only grab titles > 6

--- to-do-api/index.js
+++ to-do-api/index.js
@@ -1,17 +1,39 @@
 const api_url='https://jsonplaceholder.typicode.com/posts';
 
-async function getResponse(api_url) {
+function getResponse(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log("Error. Status Code: " + response.status);
+                return; 
+            }
+
+            response.json().then(data => {
+                resolve(data);
+            });
+        })
+        .catch(function(err) {
+            console.log("Fetch Error:", err);
+            reject(err);
+        });
+    })
     
-    // Store response
-    const response = await fetch(api_url);
-
-    // Store data in form of JSON
-    var data = await response.json();
-
-    console.log(data);
 };
 
-getResponse(api_url);
+async function useData() {
+    const data = await getResponse(api_url);
+
+    data.forEach(el => {
+        let result = el.title.split(" ");
+
+        if (result.length > 6) {
+            console.log(el.id);
+        }
+    });
+}
+
+useData();
 // Get all titles with values > 6
 
 
