822b5a294b3b036ab6124b7b8bba9c22c3135b79
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-27 23:13:14 +0100

    Moved validation to its own component

--- phone-directory/scripts/index.js
+++ phone-directory/scripts/index.js
@@ -19,59 +19,6 @@ function clearFields() {
 // TODO: Fix ERROR DIV containers
 // TODO: Improve no results div
 
-// Validation
-document.querySelector('#phone-form').addEventListener('submit', (e) => {
-    e.preventDefault(); // This stops re-rendering the error divs for some reason
-
-    // Grab all user input
-    const name = document.querySelector('#name').value;
-    const num = document.querySelector('#num').value;
-    const email = document.querySelector('#email').value;
-    
-    // If any of the fields are empty:
-    if (name === '' || num == '' || email == '') {
-        PhoneDirectory.Alert("error");
-        PhoneDirectory.clearFields(); 
-    // If name is > 20
-    } else if (name.length > 20 ) {
-        console.log('name-error: > 20');
-        PhoneDirectory.Alert("name-error");
-        PhoneDirectory.clearFields();
-    // If name contains !(alpha/space only)
-    } else if (!nameReg.test(name)) {
-        console.log('name-error: !regex');
-        PhoneDirectory.Alert("name-error"); // TODO: Create a different val error for this
-        PhoneDirectory.clearFields(); 
-    // If number passed is not a number || not equal to 10 
-    } else if (isNaN(num) || num.length != 10) {
-        console.log('num-error: !num || != 10');
-        PhoneDirectory.Alert("num-error");
-        PhoneDirectory.clearFields();
-        PhoneDirectory.clearFields();
-    // If email passed is not a valid email || >= 40 in length
-    } else if (email.length >= 40 || emailReg.test(email)) {
-        console.log('email-error: !regex || >= 40 ');
-        PhoneDirectory.Alert("email-error");
-        PhoneDirectory.clearFields(); 
-    } else {
-        // Create instance of the class
-        const book = new Phone(name, num, email);
-
-        PhoneDirectory.addBook(book);
-        Directory.addBook(book);
-
-        alert('Details added sucessfully');
-
-        // Resets all error messages
-        var divs = document.getElementsByClassName("response");
-        for (let i = 0; i < divs.length; i++) {
-            divs[i].style.display = 'none';
-        }
-
-        PhoneDirectory.clearFields();
-    }
-});
-
 function findBook2() {
 
     clearFields();
@@ -126,17 +73,14 @@ function findBook2() {
 
 // TODO: Make every odd row colored
 function sortTable(n) {
-    var table;
-    var rows;
-    var switching;
-    var i;
-    var x;
-    var y;
+    var table, rows;
+    var switching = true;
+    var i, x, y;
     var shouldSwitch;
     var dir;
     var switchCount = 0;
+
     table = document.getElementById("contacts");
-    switching = true;
 
     // Set sorting direction to asc
     dir = "asc";
@@ -180,4 +124,7 @@ function sortTable(n) {
             } 
         };
     } 
+
+
+
 };
