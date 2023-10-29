765028826a5ad020aa9f2302c688381296e5f712
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-29 19:03:41 +0000

    Fixed email validation bug

--- phone-directory/scripts/components/validation.js
+++ phone-directory/scripts/components/validation.js
@@ -1,7 +1,8 @@
 // TODO: FIX EMAIL REG
 // Regex values
 const nameReg = /^[a-zA-Z\s]+$/;
-const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// const emailReg2 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;    // This regex allows a dot at the beginning and end
 
 // Validation
 document.querySelector('#phone-form').addEventListener('submit', (e) => {
@@ -24,7 +25,7 @@ document.querySelector('#phone-form').addEventListener('submit', (e) => {
     // If name contains !(alpha/space only)
     } else if (!nameReg.test(name)) {
         console.log('name-error: !regex');
-        PhoneDirectory.Alert("name-error"); // TODO: Create a different val error for this
+        PhoneDirectory.Alert("name-error"); 
         PhoneDirectory.clearFields(); 
     // If number passed is not a number || not equal to 10 
     } else if (isNaN(num) || num.length != 10) {
@@ -32,9 +33,13 @@ document.querySelector('#phone-form').addEventListener('submit', (e) => {
         PhoneDirectory.Alert("num-error");
         PhoneDirectory.clearFields();
         PhoneDirectory.clearFields();
-    // If email passed is not a valid email || >= 40 in length
-    } else if (email.length >= 40 || !(emailReg.test(email))) {
-        console.log('email-error: !regex || >= 40 ');
+    // If email passed is >= 40 in length
+    } else if (email.length >= 40) {
+        console.log('email-error: >= 40 ');
+        PhoneDirectory.Alert("email-error");
+        PhoneDirectory.clearFields(); 
+    } else if (!(emailReg.test(email))) {
+        console.log('email-error: !regex');
         PhoneDirectory.Alert("email-error");
         PhoneDirectory.clearFields(); 
     } else {
