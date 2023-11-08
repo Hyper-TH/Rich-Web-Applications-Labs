af5bfe3baf6e7a5854e50c19dd3d153b9f4d3f2f
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-08 09:44:33 +0000

    Console log response

--- github-api/src/scripts/components/request.js
+++ github-api/src/scripts/components/request.js
@@ -17,6 +17,7 @@ function getResponse(url) {
             }
             response.json().then(data => {
                 resolve(data);
+                console.log(data);
             });
         })
         .catch(function(err) {
