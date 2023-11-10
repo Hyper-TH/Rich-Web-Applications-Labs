7d6fd1f5840155451e0abb5bd08ac4f228cf3056
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-10 16:37:56 +0000

    Moved components

--- notes-rxjs/src/scripts/index.js
+++ notes-rxjs/src/scripts/index.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const { range, filter, map, Observable, fromEvent, interval } = rxjs;
 
-    // interval observer
-    const interval$ = interval(1000);
+    const notesContainer = document.getElementById("app");
+    const addNoteButton = notesContainer.querySelector(".add-note");
 
-    interval$.subscribe(val => console.log(val));
+    // FOr each note, create an element for them
+    getNotes().forEach((note) => {
+        const noteElement = createNoteElement(note.id, note.content, note.color);
+        notesContainer.insertBefore(noteElement, addNoteButton);
+    });
 
+    // TODO: CONVERT TO RXJS (Observer)
+    // Event listener for pressing plus button 
+    addNoteButton.addEventListener("click", () => addNote());
+
+        // interval observer
+        // const interval$ = interval(1000);
+
+        // interval$.subscribe(val => console.log(val));
 });
