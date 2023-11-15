5dfb14a39535f37891d6b051f625b384ba9b7894
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-15 13:40:31 +0000

    Now added comments and the hours

--- countdown/src/scripts/index.js
+++ countdown/src/scripts/index.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const { fromEvent, interval, takeUntil, switchMap, tap } = rxjs;
 
+    const hoursInput = document.getElementById('hour');
     const minutesInput = document.getElementById('minute');
     const secondsInput = document.getElementById('second');
+
     const startButton = document.getElementById('start');
     const stopButton = document.getElementById('stop');
     const renderTimer = document.getElementById('timer');
@@ -10,24 +12,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const start$ = fromEvent(startButton, 'click');
     const stop$ = fromEvent(stopButton, 'click');
 
-    // Combine minutes and seconds observables
+    // Switchmap to switch to a new observable when start is activated 
     start$.pipe(
         switchMap(() => {
+            // Extract DOM values and convert them to integers (default to 0 if none)
+            const hours = parseInt(hoursInput.value) || 0;
             const minutes = parseInt(minutesInput.value) || 0;
             const seconds = parseInt(secondsInput.value) || 0;
 
+            const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+            
+            // interval observer for countdown
             return interval(1000).pipe(
-                takeUntil(stop$),
+                takeUntil(stop$),   // takeUntil either button is clicked (TODO: these could be merged)
                 takeUntil(start$),
+                // Tap into observable to perform side effects (logging and rendering) so that it does not affect the values
                 tap(countdown => {
-                    const totalSeconds = minutes * 60 + seconds - countdown;
-                    const displayMinutes = Math.floor(totalSeconds / 60);
-                    const displaySeconds = totalSeconds % 60;
+                    const remainingSeconds = totalSeconds - countdown;
 
-                    console.log(`${displayMinutes}M : ${displaySeconds}S Countdown: ${countdown}`);
-                    renderTimer.textContent = `${displayMinutes}M : ${displaySeconds}S Countdown: ${countdown}`;
+                    const displayHours = Math.floor(remainingSeconds / 3600);
+                    const displayMinutes = Math.floor((remainingSeconds % 3600) / 60);
+                    const displaySeconds = remainingSeconds % 60;
+
+                    console.log(`${displayHours}H : ${displayMinutes}M : ${displaySeconds}S Countdown: ${countdown}`);
+                    renderTimer.textContent = `${displayHours}H : ${displayMinutes}M : ${displaySeconds}S Countdown: ${countdown}`;
                 })
             );
         })
-    ).subscribe();
+    ).subscribe();  // subscribe to the observable created by the switchMap
 });
