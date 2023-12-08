af08a6c2cbf3ef58e7ae06f85ab0d8c5e44183b4
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-08 03:18:57 +0000

    Added routes

--- problem-2/notes-app/src/App.js
+++ problem-2/notes-app/src/App.js
@@ -1,55 +1,41 @@
 import './App.css';
-import { useState } from 'react';
-import { Note } from './Note';
+import {
+	BrowserRouter as
+	Router,
+	Routes,
+	Route
+} from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { NotesPage } from './pages/NotesPage';
+import { CatFactsPage } from './pages/CatFactsPage';
 
 function App() {
-	const [notesList, setNoteList] = useState([]);
-	const [newNote, setNewNote] = useState();
-
-	const handleChange = (event) => {
-		setNewNote(event.target.value);
-	};
-
-	const addNote = () => {
-		const note = {
-			id: notesList.length === 0 ? 1 : notesList[notesList.length - 1].id + 1,
-			noteName: newNote,
-			complete: false
-		}
-
-		setNoteList([...notesList, note])
-	};
-
-
-	const deleteNote = (id) => {
-		setNoteList(notesList.filter((note) => note.id !== id));
-	};
-
-	const updateNote = (id) => {
-		setNoteList(notesList.map((note) => { return note.id === id ? {...note, completed: true} : note;}))
-	};
-
 	return (
-		<div className="App">
-			<div className="addNote">
-				<input type='text' onChange={handleChange} />
-				<button onClick={addNote}>Add Note</button>
-			</div>
-
-			<div className="container">
-				{notesList.map((note) => {
-					return (
-						<Note	
-							noteName={note.noteName}
-							id={note.id}
-							completed={note.completed}
-							deleteNote={deleteNote}
-							updateNote={updateNote}
+		<>
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route 
+					path="/notes" 
+					element={
+						<NotesPage 
+							subPageName="NotesPage"
+							backTo="/"
 						/>
-					);
-				})}
-			</div>
-		</div>
+					} 
+				/>
+				<Route 
+					path="/catfacts" 
+					element={
+						<NotesPage 
+							subPageName="CatFactsPage"
+							backTo="/"
+						/>
+					} 
+				/>
+			</Routes>
+		</Router>
+		</>
 	);
 }
 
