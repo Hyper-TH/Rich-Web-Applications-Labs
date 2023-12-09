ddbbb9fc3e563b35d3969a958cc8828a03eaa72e
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-09 23:12:50 +0000

    Added update functionality

--- problem-2/notes-app/src/components/Note.js
+++ problem-2/notes-app/src/components/Note.js
@@ -1,15 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import '../styles/NotesPage.css';
 
 export const Note = (props) => {
+    const [noteContent, setNoteContent] = useState(props.noteName);
+
+    const handleChange = (event) => {
+        setNoteContent(event.target.value);
+    };
+
+    const handleDoubleClick = async () => {
+        await props.deleteNote(props.id);
+    };
+
+    const handleSaveNote = async () => {
+        try {
+            await Axios.post('http://localhost:8000/updateNote', {
+                id: props.id,
+                content: noteContent,
+            });
+
+            console.log('Note updated successfully');
+        } catch (error) {
+            console.error(`Axios Error: ${error}`);
+        }
+    };
+
+    useEffect(() => {
+        setNoteContent(props.noteName);
+    }, [props.noteName]);
 
     return (
         <>
-        <textarea 
-            className="note"
-            value={props.noteName}
-            style={{ backgroundColor: `rgb${props.color}` }}
-            onDoubleClick={() => props.deleteNote(props.id)}
-        />
+            <textarea
+                className="note"
+                value={noteContent}
+                style={{ backgroundColor: `rgb${props.color}` }}
+                onChange={handleChange}
+                onBlur={handleSaveNote}
+                onDoubleClick={handleDoubleClick}
+            />
         </>
     );
 };
