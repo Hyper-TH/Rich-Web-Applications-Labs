93b6d5406811b4425057d7f95f98a8121591f457
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-09 14:49:20 +0000

    Able to GET and PUT from firestore for notes

--- problem-2/notes-app/server/server.js
+++ problem-2/notes-app/server/server.js
@@ -16,12 +16,58 @@ admin.initializeApp({
     databaseURL: `https://${database_id}.firebaseio.com/`
 });
 
+const firestore = admin.firestore();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/message', (req, res) => {
     res.json({ message: 'Hello from server!' });
+});
+
+// Endpoint to get notes from Firestore
+app.get('/getNotes', async (req, res) => {
+    try {
+        const collectionName = 'Notes';
+
+        // Fetch all docuemnts from the "notes" collection
+        const querySnapshot = await firestore.collection(collectionName).get();
+
+        // Extract document data
+        const documents = querySnapshot.docs.map((doc) => doc.data());
+
+        res.json({ documents });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: `Firebase Server Error` });
+    }
+});
+
+// Endpoint to put new notes in Firestore
+app.post('/putNotes', async (req, res) => {
+    try {
+        const { id, content, color } = req.body;
+
+        if (!content) {
+            return res.status(400).json({ error: `Note requires content to be added` })
+        }
+
+        const collectionName = 'Notes';
+
+        const data = {
+            id,
+            content,
+            color
+        };
+
+        // Add the note to the notes collection in Firestore
+        await firestore.collection(collectionName).add(data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: `Firebase Server Error` });
+    }
 });
 
 // Start server
