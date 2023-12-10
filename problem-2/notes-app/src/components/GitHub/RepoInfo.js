0bbc1b44bd03278e34addb14cb1d3854c7872eed
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-10 16:49:00 +0000

    Fixed warning "Encountered two children with same key..."

--- problem-2/notes-app/src/components/GitHub/RepoInfo.js
+++ problem-2/notes-app/src/components/GitHub/RepoInfo.js
@@ -1,30 +1,32 @@
 import '../../styles/GitHubPage.css';
+import React from 'react';
 
-// TODO: FIX ERROR REGARDING MAPS
 export const RepoInfo = (props) => {
-    return <>    
-      <div id='userRepos'>
-            <h1> User Repos </h1> 
-            <div id="repoTableWrapper">
-                <table id='repoInfo'>
-                    <tbody id="Rlist">
-                        {props.repoInfo.map(function(repo,i) {
-                            return (
-                            <>
-                                <tr key={i}>
-                                    <th>Name:</th>
-                                    <td>{repo.name}</td>
-                                </tr>
-                                <tr key={i}>
-                                    <th>Description:</th>
-                                    <td>{repo.description || 'N/A'}</td>
-                                </tr>
-                            </>
-                            )
-                        })}
-                    </tbody>
-                </table>
+    return (
+        <>
+            <div id='userRepos'>
+                <h1> User Repos </h1>
+                <div id="repoTableWrapper">
+                    <table id='repoInfo'>
+                        <tbody id="Rlist">
+                            {props.repoInfo.map(function (repo, i) {
+                                return (
+                                    <React.Fragment key={i}>
+                                        <tr>
+                                            <th>Name:</th>
+                                            <td>{repo.name}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Description:</th>
+                                            <td>{repo.description || 'N/A'}</td>
+                                        </tr>
+                                    </React.Fragment>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    </>
+        </>
+    );
 };
