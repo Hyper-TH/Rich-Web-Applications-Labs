823dfa03766dfebfcfbeb9768b30243e38aabb75
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-10 00:14:19 +0000

    Fetch and render a cat fact

--- problem-2/notes-app/src/pages/CatFactsPage.js
+++ problem-2/notes-app/src/pages/CatFactsPage.js
@@ -1,15 +1,36 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { Axios } from 'axios';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
 import { useState, useEffect } from 'react';
+import '../styles/CatPage.css';
 
 export const CatFactsPage = ({subPageName, backTo}) => {
+    const [catFact, setCatFact] = useState("");
+
+    const fetchCatFact = () => {
+        Axios.get("https://catfact.ninja/fact").then((res) => {
+            setCatFact(res.data.fact);
+        });
+    };
+    // Run only when the component mounts
+    useEffect(() => {
+        fetchCatFact();
+    }, []);
+
     return (
         <>
         <h1>Welcome to the {subPageName}</h1>
 
-        <button>
+        <button className='button'>
             <Link to={backTo}>Back to Home</Link>
         </button>
+
+        <div className="App"> 
+            <button onClick={fetchCatFact} className='button'>
+                Generate Cat Fact
+            </button>
+            <h2 className='catFact'> {catFact} </h2>
+		</div>
+
         </>
     );
 };
